e31f23603eade1184fc610d6dd0d6bdc520f0649
imCsCorp <camilo.alejandro.soto.v@gmail.com>
2023-04-25 01:15:34 -0500

    page 404 test

---
+++ test/pages/NotFoundPage.test.jsx
@@ -0,0 +1,36 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+import NotFoundPage from "../../src/pages/NotFoundPage";
+
+describe("<NotFoundPage />", () => {
+  test("should be mounted", () => {
+    const { getByTestId } = render(
+      <HelmetProvider>
+        <MemoryRouter initialEntries={["/404"]}>
+          <Routes>
+            <Route path="/404" element={<NotFoundPage />} />
+          </Routes>
+        </MemoryRouter>
+      </HelmetProvider>
+    );
+    expect(getByTestId("title").innerHTML).toContain("404");
+  });
+
+  test("should be redirected", () => {
+    const { getByTestId } = render(
+      <HelmetProvider>
+        <MemoryRouter initialEntries={["/404"]}>
+          <Routes>
+            <Route path="/" element={<p data-testid="redirect">Home</p>} />
+            <Route path="/404" element={<NotFoundPage />} />
+          </Routes>
+        </MemoryRouter>
+      </HelmetProvider>
+    );
+    const button = getByTestId("go-home");
+    fireEvent.click(button);
+    expect(getByTestId("redirect").innerHTML).toContain("Home");
+  });
+});
