61edd27fbb2980b6d39bae68600c1e31b6ff8ef6
imCsCorp <camilo.alejandro.soto.v@gmail.com>
2023-04-25 01:03:32 -0500

    pagina 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Helmet } from "react-helmet-async";
 
@@ -6,9 +7,33 @@ const NotFoundPage = () => {
   return (
     <React.Fragment>
       <Helmet>
-        <title>Home | CSCORP ECOMMERCE</title>
+        <title>Ups | CSCORP ECOMMERCE</title>
       </Helmet>
-      <p>NotFoundPage</p>
+      <React.Fragment>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+          <div className="text-center">
+            <h1 className="display-1 fw-bold" data-testid="title">
+              404
+            </h1>
+            <p className="fs-3">
+              <span data-testid="test-msg" className="text-danger">
+                Opps!
+              </span>{" "}
+              Pagina no encontrada.
+            </p>
+            <p className="lead">
+              La pagina a la cual quieres acceder no existe.
+            </p>
+            <Link
+              className="btn btn-primary"
+              to="/"
+              data-testid="go-home"
+            >
+              Ir al inicio
+            </Link>
+          </div>
+        </div>
+      </React.Fragment>
     </React.Fragment>
   );
 };
